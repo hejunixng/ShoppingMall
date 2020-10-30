c42229912f1f08564a0e8b9479ef43b92e0296a8
hejunxing <1063378763@qq.com>
2020-10-30 21:31:51 +0800

    vue.config,js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    lintOnSave: false,
+    lintOnSave: false,//eslint关闭
     devServer: {
         host: 'localhost',
         port: 8080,
@@ -13,7 +13,9 @@ module.exports = {
             }
         }
     },
-    productionSourceMap:false,
+    productionSourceMap:false,//sourcema关闭
+    //兼容其他浏览器，prefetech的JS文件是wepback打包时懒加载生成的一个地址，浏览器会在空闲时间解析，
+    //下面是彻底不要prefetch文件,真正的按需加载
     chainWebpack:(config)=>{
         config.plugins.delete('prefetch')
     }
